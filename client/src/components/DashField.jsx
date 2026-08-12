f29c7c06ea5bae9546e962d0ab0eac5755ccae29
alloy01 <ajeettsingh51@gmail.com>
2026-08-12 22:49:35 +0530

    refactor: to avoid typos modify states value can be selected from a object "modifyModes".

--- client/src/components/DashField.jsx
+++ client/src/components/DashField.jsx
@@ -43,6 +43,13 @@ const DashField = ({ setLoadedDocs }) => {
 
     // modify state like "edit item" will help us to what input fields to show when user clicks modify buttons
     const [modifyState,setModifyState] = useState(null);
+    // modes to avoid typos
+    const modifyMode ={
+        ADD: "add",
+        EDIT: "edit",
+        FILTER: "filter",
+        DELETE: "delete"
+    }
 
     // handle submit button
     const handleSubmit = async(e) => {
@@ -69,11 +76,11 @@ const DashField = ({ setLoadedDocs }) => {
 
                 // reload the window when api is successful but if we reload the page when data for filter arrives then it vanishes so we don't reload
 
-                if(response.data.success && modifyState != 'filter'){
+                if(response.data.success && modifyState != modifyMode.FILTER){
                     window.location.reload()
                 }
                 
-                if(modifyState == 'filter'){
+                if(modifyState == modifyMode.FILTER){
                     setLoadedDocs(response.data.payload)
                 }
 
@@ -130,41 +137,41 @@ const DashField = ({ setLoadedDocs }) => {
                 </div>
                 <div className="text-center text-stone-200 mt-4 grid grid-cols-1 lg:grid-cols-2 lg:gap-x-2">
                     <button className="cursor-pointer mb-2 px-2 border-2" onClick={() => {
-                        if(modifyState === "add"){
+                        if(modifyState === modifyMode.ADD){
                             setModifyState(null);
                         }
                         else{
-                            setModifyState("add");  
+                            setModifyState(modifyMode.ADD);  
                         }
                     }}>
                     {`Add item >`}
                     </button>
                     <button className="cursor-pointer mb-2 px-2 border-2" onClick={() => {
-                        if(modifyState === "edit"){
+                        if(modifyState === modifyMode.EDIT){
                             setModifyState(null);
                         }
                         else{
-                            setModifyState("edit");  
+                            setModifyState(modifyMode.EDIT);  
                         }
                     }}>
                     {`Edit item >`}
                     </button>
                     <button className="cursor-pointer mb-2 px-2 border-2" onClick={() => {
-                        if(modifyState === "filter"){
+                        if(modifyState === modifyMode.FILTER){
                             setModifyState(null);
                         }
                         else{
-                            setModifyState("filter");  
+                            setModifyState(modifyMode.FILTER);  
                         }
                     }}>
                     {`Filter item >`}
                     </button>
                     <button className="cursor-pointer mb-2 px-2 border-2" onClick={() => {
-                        if(modifyState === "delete"){
+                        if(modifyState === modifyMode.DELETE){
                             setModifyState(null);
                         }
                         else{
-                            setModifyState("delete");  
+                            setModifyState(modifyMode.DELETE);  
                         }
                     }}>
                     {`Delete item >`}
@@ -179,25 +186,25 @@ const DashField = ({ setLoadedDocs }) => {
                             <p className="text-center text-stone-200">{modifyState} item</p>
                         </div>
                         <div className="px-4 flex flex-col gap-y-2">
-                            <input type="text" name="item_name" placeholder="enter item name." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == "filter" ? "hidden" : "block"}`} value={formData.item_name} onChange={handleChange}/>
+                            <input type="text" name="item_name" placeholder="enter item name." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == modifyMode.FILTER ? "hidden" : "block"}`} value={formData.item_name} onChange={handleChange}/>
 
-                            <input type="number" name="item_quantity" placeholder="enter quantity." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == "add" || modifyState == "edit" ? "block" : "hidden"}`} value={formData.item_quantity} onChange={handleChange}/>
+                            <input type="number" name="item_quantity" placeholder="enter quantity." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == modifyMode.ADD || modifyState == modifyMode.EDIT ? "block" : "hidden"}`} value={formData.item_quantity} onChange={handleChange}/>
 
-                            <input type="text" name="item_category" placeholder="enter category." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == "add" || modifyState == "edit" ? "block" : "hidden"}`} value={formData.item_category} onChange={handleChange}/>
+                            <input type="text" name="item_category" placeholder="enter category." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == modifyMode.ADD || modifyState == modifyMode.EDIT ? "block" : "hidden"}`} value={formData.item_category} onChange={handleChange}/>
 
-                            <input type="text" name="item_supplier" placeholder="enter supplier." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == "add" || modifyState == "edit" ? "block" : "hidden"}`} value={formData.item_supplier} onChange={handleChange}/>
+                            <input type="text" name="item_supplier" placeholder="enter supplier." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == modifyMode.ADD || modifyState == modifyMode.EDIT ? "block" : "hidden"}`} value={formData.item_supplier} onChange={handleChange}/>
 
-                            <input type="text" name="item_costprice" placeholder="enter costprice." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == "add" || modifyState == "edit" ? "block" : "hidden"}`} value={formData.item_costprice} onChange={handleChange}/>
+                            <input type="text" name="item_costprice" placeholder="enter costprice." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == modifyMode.ADD || modifyState == modifyMode.EDIT ? "block" : "hidden"}`} value={formData.item_costprice} onChange={handleChange}/>
 
-                            <input type="text" name="item_desc" placeholder="enter desc." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == "add" || modifyState == "edit" ? "block" : "hidden"}`} value={formData.item_desc} onChange={handleChange}/>
+                            <input type="text" name="item_desc" placeholder="enter desc." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == modifyMode.ADD || modifyState == modifyMode.EDIT ? "block" : "hidden"}`} value={formData.item_desc} onChange={handleChange}/>
 
-                            <input name="item_status" placeholder="enter status." id="item_status" className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == "add" || modifyState == "edit" ? "block" : "hidden"}`} value={formData.item_status} onChange={handleChange}/>
+                            <input name="item_status" placeholder="enter status." id="item_status" className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == modifyMode.ADD || modifyState == modifyMode.EDIT ? "block" : "hidden"}`} value={formData.item_status} onChange={handleChange}/>
 
-                            <input type="text" name="item_unit" placeholder="enter unit." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == "add" || modifyState == "edit" ? "block" : "hidden"}`} value={formData.item_unit} onChange={handleChange}/>
+                            <input type="text" name="item_unit" placeholder="enter unit." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == modifyMode.ADD || modifyState == modifyMode.EDIT ? "block" : "hidden"}`} value={formData.item_unit} onChange={handleChange}/>
 
-                            <input type="text" name="item_filter_field" placeholder="enter field." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == "filter" ? "block" : "hidden"}`} value={formData.item_filter_field} onChange={handleChange}/>
+                            <input type="text" name="item_filter_field" placeholder="enter field." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == modifyMode.FILTER ? "block" : "hidden"}`} value={formData.item_filter_field} onChange={handleChange}/>
 
-                            <input type="text" name="item_filter_param" placeholder="enter parameter." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == "filter" ? "block" : "hidden"}`} value={formData.item_filter_param} onChange={handleChange}/>
+                            <input type="text" name="item_filter_param" placeholder="enter parameter." className={`outline-0 py-1 text-slate-300 font-mono w-max ${modifyState == modifyMode.FILTER ? "block" : "hidden"}`} value={formData.item_filter_param} onChange={handleChange}/>
                         </div>
 
                         <div className="min-w-max flex justify-center mt-4">
