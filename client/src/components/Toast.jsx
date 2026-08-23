675f5070c1458331390cc97aa3a0d338f58e7299
alloy01 <ajeettsingh51@gmail.com>
2026-08-23 23:32:23 +0530

    style: toast appears close to right side on devices with width less than sm

--- client/src/components/Toast.jsx
+++ client/src/components/Toast.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const Toast = ({ toastBlock,toastMessage }) => {
 
 	return(
-		<div className={`text-stone-200 absolute max-w-96 bg-black/70 -right-100 transition-all duration-300 font-mono border-2 border-stone-200 top-12 px-6 py-2 ${toastBlock ? '-translate-x-108' : 'translate-x-0'}`}>
+		<div className={`text-stone-200 absolute max-w-96 bg-black/70 -right-104 sm:-right-96 transition-all duration-300 font-mono border-2 border-stone-200 top-12 px-6 py-2 ${toastBlock ? '-translate-x-108' : 'translate-x-0'}`}>
 			{ toastMessage }
 		</div>
 	)
